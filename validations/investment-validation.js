640ee35edcaf0199052738780f68c1e04ff88bbd
DumithLusena <dumithlusena123@gmail.com>
2025-12-11 19:39:40 +0530

    feat: Add investment request information endpoint and ongoing cultivations retrieval

--- validations/investment-validation.js
+++ validations/investment-validation.js
@@ -1,5 +1,5 @@
 const Joi = require("joi");
 
-exports.getAllInvestmentSchema = Joi.object({
-  // No query params currently. Add pagination later if needed
+exports.getInvestmentRequestSchema = Joi.object({
+  requestId: Joi.number().integer().positive().required()
 });
