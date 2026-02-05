b6553383b4137c6a571b1cdcb746524aed82e3d9
Pasan-FDo <pasanfernando.work@gmail.com>
2026-02-05 16:46:12 +0530

    style: Refactor route definitions for improved readability in Auth.js

--- routes/Auth.js
+++ routes/Auth.js
@@ -6,15 +6,43 @@ const router = express.Router();
 
 router.get("/test", authEp.test);
 
-router.post('/login', authEp.userLogin);
-router.post('/register', authEp.registerUser);
-router.post('/check-user', authEp.checkUserExists);
+router.post(
+    '/login',
+    authEp.userLogin
+);
+router.post(
+    '/register',
+    authEp.registerUser
+);
+router.post(
+    '/check-user',
+    authEp.checkUserExists
+);
 
 // Password reset routes
-router.post("/forgot-password", authEp.forgotPassword); // Send reset email
-router.get("/validate-reset-token/:token", authEp.validateResetToken); // Validate token
-router.put("/reset-password", authEp.resetPassword); // Reset password
-router.post("/check-phone", authEp.checkPhoneNumber);
-router.post("/reset-password-by-phone", authEp.resetPasswordByPhone);
+router.post(
+    "/forgot-password",
+    authEp.forgotPassword
+); // Send reset email
+
+router.get(
+    "/validate-reset-token/:token",
+    authEp.validateResetToken
+); // Validate token
+
+router.put(
+    "/reset-password",
+    authEp.resetPassword
+); // Reset password
+
+router.post(
+    "/check-phone",
+    authEp.checkPhoneNumber
+);
+
+router.post(
+    "/reset-password-by-phone",
+    authEp.resetPasswordByPhone
+);
 
 module.exports = router;
